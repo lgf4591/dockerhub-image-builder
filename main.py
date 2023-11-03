4d00d98acd8e50a1d0d99b05c8777212fcc8cc40
lgf4591 <lgf4591@outlook.com>
2023-11-04 03:52:09 +0800

    test  Docker Image CI/CD Template

--- main.py
+++ main.py
@@ -70,6 +70,32 @@ RUN {} update && export DEBIAN_FRONTEND=noninteractive \
     && {} {} -y {}
 """
 
+github_cicd_template = """
+name: {}:{} Docker Image CI/CD
+on:
+  push:
+    branches: [ main ]
+    paths:   # 这里是用来指定哪个文件更改，才会触发的
+      - 'devcontainer/{}/Dockerfile.build-{}'               # 包括路径
+      - 'devcontainer/common_build.sh'                              # 包括路径
+      - '!docs/**'                                                  # 排除路径
+jobs:
+  # 构建并上传 Docker镜像
+  build: 
+    runs-on: ubuntu-latest # 依赖的环境      
+    steps:
+      - uses: actions/checkout@v2
+      - name: Build Image
+        run: |
+          docker build -t lgf4591/{}:{} -f devcontainer/{}/Dockerfile.build-{} .
+      - name: Login to Registry
+        # run: docker login --username=${{ secrets.DOCKER_USERNAME }} --password ${{ secrets.DOCKER_PASSWORD }}
+        run: echo "${{ secrets.DOCKER_PASSWORD }}" | docker login --username ${{ secrets.DOCKER_USERNAME }}  --password-stdin
+      - name: Push Image
+        run: |
+          docker push lgf4591/{}:{}
+"""
+
 # print(devcontainer_image_version_map["ubuntu"])
 
 for (image_name,image_infos) in devcontainer_image_info_test_map.items():
@@ -86,9 +112,13 @@ for (image_name,image_infos) in devcontainer_image_info_test_map.items():
         need_install_pks = image_info["need_install_pks"]
         install_pkg_verb = "add" if ("alpine" in image_name or "alpine" in image_version) else "install"
         dockerfile_content = dockerfile_template.format(image_name, image_version, pkg_mgt, pkg_mgt, install_pkg_verb, need_install_pks)
-        print(dockerfile_content)
+        # print(dockerfile_content)
         with open(f"{folder}/Dockerfile.build-{image_version}", "w") as dockerfile:
             dockerfile.write(dockerfile_content)
-    print(f"create {image_name} github actions file")
+            
+        github_cicd_template.format(image_name, image_version, image_name, image_version, image_name, image_version, image_name, image_version, image_name, image_version)
+        with open(f".github/workflows/{image_name}_{image_version}.yml", "w") as yamlfile:
+            yamlfile.write(dockerfile_content)
+    # print(f"create {image_name} github actions file")
     
 
