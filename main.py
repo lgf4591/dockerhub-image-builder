162d814c92ea63bdfbca6112b1621f75885ea4ff
lgf4591 <lgf4591@outlook.com>
2023-11-05 02:34:24 +0800

    test debian

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ devcontainer_images_need_to_build_map = {
     "debian": {
                 "common_need_install_pkgs": " curl wget ca-certificates apt-transport-https ntp bash zsh tmux csh tcsh ksh telnet iputils-ping lsof procps git openssh-server net-tools vim make cmake automake ",
                 "pkg_mgt": " apt-get ",
-                "change_mirrir_command": "cp /etc/apt/sources.list /etc/apt/sources.list.bak && sed -i 's#http://deb.debian.org#https://mirrors.ustc.edu.cn#g' /etc/apt/sources.list && sed -i 's|security.debian.org/debian-security|mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list ",
+                "change_mirrir_command": "cp /etc/apt/sources.list /etc/apt/sources.list.bak && sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list ",
                 "image_info_dict": 
                     [
                         {
